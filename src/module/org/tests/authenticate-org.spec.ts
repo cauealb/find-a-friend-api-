5cbb6fb794056e7d391e4c8a5be5c9ec158e008f
cauealb <cauealvesb4@gmail.com>
2026-08-18 23:01:33 -0300

    test: Incluindo teste de authenticação de org

---
+++ src/module/org/tests/authenticate-org.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import type { orgRepository } from "../../../repositories/org-repository.ts";
+import { AuthenticateOrg } from "../use-cases/authenticate-org.ts";
+import { InMemoryOrgRepository } from "../../../repositories/in-memory/in-memory-org-repository.ts";
+
+let repository: orgRepository
+let sut: AuthenticateOrg
+
+describe("Authenticate org (unit)", () => {
+    beforeEach(() => {
+        repository = new InMemoryOrgRepository()
+        sut = new AuthenticateOrg(repository)
+    })
+
+    it("should be able authentica in system", async () => {
+        const email = 'cauealvesdev@gmail.com'
+        const password = '1234567'
+
+        const user = repository.create({
+            nameOrg: 'Cauê Alves Org',
+            email,
+            password,
+            address: 'Rua tal tal, 12',
+            number: '11999999999',
+        })
+
+        const { org } = await sut.execute({ email, password })
+
+        expect(org).toEqual(expect.objectContaining({
+            nameOrg: 'Cauê Alves Org',
+            email,
+            password,
+        }))
+    })
+})
